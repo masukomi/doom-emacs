2b43ce6c966f9c1bb5a5ba6acae62dafea29c19f
masukomi <masukomi@masukomi.org>
2020-09-21 15:36:04 -0400

    added inori_m theme converted from vim

---
+++ inori_m-theme.el
@@ -0,0 +1,127 @@
+;;; inori_m-theme.el --- Custom face theme for Emacs
+
+;; This theme was generated with vimco.el
+;; You can get it from:
+;; <https://github.com/UwUnyaa/vimco>
+
+;;; Code:
+
+(deftheme inori_m)
+
+(custom-theme-set-faces 'inori_m
+                        '(font-lock-type-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#af5f00"))))
+                        '(font-lock-function-name-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#c0c0c0"))))
+                        '(font-lock-variable-name-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#0091C0"))))
+                        '(font-lock-constant-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#008080"))))
+                        '(font-lock-preprocessor-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#d78700"))))
+                        '(font-lock-builtin-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#d78700"))))
+                        '(font-lock-keyword-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#d78700"))))
+                        '(font-lock-string-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#5f875f"))))
+                        '(font-lock-doc-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#808080"))))
+                        '(font-lock-comment-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:foreground "#808080"))))
+                        '(font-lock-warning-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:background "#800000"))))
+                        '(show-paren-match-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :foreground "#008080"))))
+                        '(default
+                           ((((class color)
+                              (min-colors 89))
+                             (:background "#262626" :foreground "#c0c0c0"))))
+                        '(highline-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t :background "#181818"))))
+                        '(ac-selection-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:background "#c0c0c0" :foreground "#262626"))))
+                        '(ac-candidate-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:background "#808080" :foreground "#262626"))))
+                        '(flyspell-duplicate
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline
+                             (:style wave)))))
+                        '(flyspell-incorrect
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline
+                             (:style wave)))))
+                        '(region
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :background "#6b8e23" :foreground "#f0e68c"))))
+                        '(mode-line-inactive
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :background "#808080" :foreground "#c0c0c0"))))
+                        '(mode-line-buffer-id
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :weight bold :background "#808080" :foreground "#c0c0c0"))))
+                        '(mode-line
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :weight bold :background "#808080" :foreground "#c0c0c0"))))
+                        '(fringe
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t :foreground "#808080"))))
+                        '(linum
+                          ((((class color)
+                             (min-colors 89))
+                            (:underline t :foreground "#808080"))))
+                        '(isearch
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t :background "#cd853f" :foreground "#f5deb3"))))
+                        '(isearch-lazy-highlight-face
+                          ((((class color)
+                             (min-colors 89))
+                            (:inverse-video t))))
+                        '(dired-directory
+                          ((((class color)
+                             (min-colors 89))
+                            (:weight bold :foreground "Cyan")))))
+
+(provide-theme 'inori_m)
+
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
